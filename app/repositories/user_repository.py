dd50bb546dad1a8d8a636b94beb61fdb71ebaa45
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-08 22:50:28 +0530

    try excpet handling

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -5,15 +5,23 @@ def get_user_by_email(db: Session, email: str) -> User | None:
     return db.query(User).filter(User.email == email).first()
 
 def create_user(db: Session, email: str, username: str, hashed_password: str) -> User:
-    try:
-        if get_user_by_email(db,email):
-            print("User Alredy exists")
-        else:
-            user = User(email=email,username=username, hashed_password=hashed_password) # Creating the Object
-            db.add(user)  # stage the object -> i want to insert it in db -> added to session's pending changes
-            db.commit()  # Actually writes the database -> now SQLAlchemy sends the SQL query to the database 
-            db.refresh(user) # Updates the python object -> the database may generate values automatically ( like id and created_at) Relod this object from the databse so it has the latest values
-            return user
-    except:
-        raise 
+    existing_user = get_user_by_email(db, email)
     
+    if existing_user:
+        raise ValueError("User already exists")
+    
+    try:
+        user = User(
+            email=email,
+            username=username,
+            hashed_password=hashed_password
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return user
+    
+    except Exception as e:
+        db.rollback()
+        raise e
